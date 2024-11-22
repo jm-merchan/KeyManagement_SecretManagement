2de96dae111826dd4c952e5d9e3d5c256b362876
jm-merchan <jose.merchan@hashicorp.com>
2024-11-22 10:45:59 +0100

    clean up

--- 1_Platform_Deployment/terraform.tf
+++ 1_Platform_Deployment/terraform.tf
@@ -1,12 +1,6 @@
 
 terraform {
   required_providers {
-    /*
-    google = {
-      source  = "hashicorp/google"
-      version = "6.3.0"
-    }
-  */
     acme = {
       source  = "vancluever/acme"
       version = "2.26.0"
@@ -14,22 +8,8 @@ terraform {
   }
 }
 
-/*
-provider "google" {
-  project = var.project_id
-}
-*/
-
-/*
-provider "google-beta" {
-  project = var.project_id
-}
-*/
-
 provider "acme" {
   server_url = local.acme_prod
-  # server_url = "https://acme-staging-v02.api.letsencrypt.org/directory" # Testing
-  # server_url = "https://acme-v02.api.letsencrypt.org/directory" # Production
 }
 
 module "vault-primary" {
